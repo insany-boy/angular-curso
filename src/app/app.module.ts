1381468ac4549ca424ee27515f922b1ed73ebda9
insany-boy <kauanmeirelles1008@gmail.com>
2024-02-29 15:23:09 -0300

    refactor: alter produtos.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
